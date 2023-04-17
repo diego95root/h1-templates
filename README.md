3238863a705834c588f68833f334a086e0799f9e
Diego <diegobernal0905@gmail.com>
2023-04-17 11:48:35 +0200

    Add version 1.0.0

---
+++ README.md
@@ -0,0 +1,27 @@
+# HackerOne Templates
+
+HackerOne Templates is a browser extension that enhances the reporting experience on HackerOne by providing helpful tools and templates for creating reports.
+
+## Installation
+
+1. Download the latest release of the extension from the [releases page](https://github.com/diego95root/h1-templates/releases).
+2. Extract the downloaded archive.
+3. Open Firefox and navigate to `about:debugging`.
+4. Click the "This Firefox" tab, then click the "Load Temporary Add-on" button.
+5. In the file dialog, select the `manifest.json` file from the extracted archive.
+
+## Usage
+
+Once installed, the extension will automatically activate on the "New Report" page of any HackerOne program. It provides the following features:
+
+-   A button to upload a report template in Markdown format.
+-   A dropdown menu with pre-made templates that can be inserted into the report.
+-   A button to clear the entire report form.
+
+## Contributing
+
+Pull requests are welcome! For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+This project is licensed under the [MIT](https://choosealicense.com/licenses/mit/) license.
---
+++ content.js
@@ -0,0 +1,215 @@
+document.addEventListener('readystatechange', function (event) {
+    if (event.target.readyState === 'complete') {
+        const observer = new MutationObserver(function (mutations) {
+            mutations.forEach(function (mutation) {
+                if (mutation.type === 'childList') {
+                    mutation.addedNodes.forEach(function (node) {
+                        if (
+                            node.classList &&
+                            node.classList.contains('daisy-grid')
+                        ) {
+                            observer.disconnect();
+                            addReportElement(node);
+                        }
+                    });
+                }
+            });
+        });
+        observer.observe(document.documentElement, {
+            childList: true,
+            subtree: true,
+        });
+    }
+});
+
+function createTemplateBtn(report) {
+    const fileEl = document.createElement('div');
+    // Make it nice!
+    fileEl.classList =
+        'selected-file daisy-button daisy-button--secondary daisy-button--small daisy-button-- daisy-button--daisys-container delay-0';
+    fileEl.textContent = report.fileName;
+    fileEl.style.marginRight = '5px';
+    fileEl.style.marginBottom = '5px';
+
+    fileEl.addEventListener('click', () => {
+        fileEl.classList.replace(
+            'daisy-button--secondary',
+            'daisy-button--primary'
+        );
+        const selectedFiles = document.querySelectorAll('.selected-file');
+        selectedFiles.forEach((selectedFile) => {
+            if (selectedFile !== fileEl) {
+                selectedFile.classList.remove('daisy-button--primary');
+                selectedFile.classList.add('daisy-button--secondary');
+            }
+        });
+
+        let parsedReport = parseMarkdown(report.content);
+        console.log(parsedReport);
+
+        setKeywordText('', '#report_title');
+        setKeywordText('', '#report_vulnerability_information');
+        setKeywordText('', '#report_impact');
+        parsedReport.forEach((section) => {
+            if (section.name == 'Impact') {
+                setKeywordText(section.content.trim(), '#report_impact');
+            } else if (section.name == 'Title') {
+                setKeywordText(section.content.trim(), '#report_title');
+            } else if (section.name == 'Severity') {
+                console.log(`button[value="${section.content.trim()}"]`);
+                document
+                    .querySelector(
+                        `.spec-rating-${section.content.trim().toLowerCase()}`
+                    )
+                    ?.click();
+            } else {
+                setKeywordText(
+                    '## ' + section.name + '\n\n' + section.content.trim(),
+                    '#report_vulnerability_information'
+                );
+            }
+        });
+    });
+    return fileEl;
+}
+
+function parseMarkdown(content) {
+    const sections = [];
+    let currentSection = null;
+
+    content.split('\n').forEach((line) => {
+        if (line.startsWith('## ')) {
+            const heading = line.substr(3).trim();
+            currentSection = { name: heading, content: '' };
+            sections.push(currentSection);
+        } else if (currentSection) {
+            currentSection.content += line + '\n';
+        }
+    });
+
+    return sections;
+}
+
+function setKeywordText(text, id) {
+    var el = document.querySelector(id);
+    if (el.value === '' || text === '') el.value = text;
+    else el.value = el.value + '\n\n' + text;
+    var evt = document.createEvent('Events');
+    evt.initEvent('change', true, true);
+    el.dispatchEvent(evt);
+}
+
+function addReportElement(timeline) {
+    const parentEl = document.querySelector('.daisy-timeline--medium');
+    const reportList = JSON.parse(localStorage.getItem('reportList'));
+    reportList?.sort((a, b) => {
+        if (a.fileName < b.fileName) return -1;
+        return 1;
+    });
+
+    const newReportEl = document.createElement('div');
+    newReportEl.classList.add('daisy-timeline__entry');
+    newReportEl.innerHTML = `
+<div class="daisy-timeline__line daisy-timeline__line--medium"></div>
+<div class="daisy-timeline__content">
+  <div class="Spacing-module_mb-spacing-32__GCnP-">
+    <div class="card">
+      <div class="card__content daisy-text">
+        <div class="interactive-markdown markdownable markdownable--with-margin-bottom spec-form-intro" style="position: relative;">
+          <div class="card__heading">
+          <div class="sc-beqWaB bpQAkP">
+          <div>
+          <strong>Templates</strong>
+          <div class="daisy-helper-text">Select the report template.</div>
+          </div>
+          <button type="button" class="upload-template-btn daisy-button daisy-button--primary daisy-button--medium daisy-button-- margin-24--left">Upload Template</button>
+          </div>
+          </div>
+          <div class="selected-files-container card__content">
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+`;
+
+    // Add the new report element to the parent element
+    parentEl.insertBefore(newReportEl, parentEl.firstChild);
+
+    const selectedFilesContainer = newReportEl.querySelector(
+        '.selected-files-container'
+    );
+    const uploadTemplateBtn = newReportEl.querySelector('.upload-template-btn');
+
+    // Check if reportList exists in local storage and contains at least one report
+    if (reportList && reportList.length > 0) {
+        // Display the filenames of loaded reports in the selected-files-container
+        reportList.forEach((report) => {
+            const fileEl = createTemplateBtn(report);
+            selectedFilesContainer.appendChild(fileEl);
+        });
+    }
+
+    // Add event listener to uploadTemplateBtn to allow uploading more templates
+    uploadTemplateBtn.addEventListener('click', () => {
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.accept = '.md';
+        input.multiple = true;
+
+        input.onchange = () => {
+            const files = input.files;
+            for (let i = 0; i < files.length; i++) {
+                const file = files[i];
+                const reader = new FileReader();
+                reader.readAsText(file);
+                reader.onload = () => {
+                    const content = reader.result;
+                    // Do something with the file content
+                    console.log(content);
+                    const report = {
+                        fileName: file.name.replace(/\.md$/, ''),
+                        content: content,
+                    };
+                    // Good that you're reading this! I wouldn't trust anyone with my report templates either :)
+                    // Thankfully they are only stored locally in local storage!
+                    let reportList =
+                        JSON.parse(localStorage.getItem('reportList')) || [];
+                    const existingIndex = reportList.findIndex(
+                        (item) => item.fileName === report.fileName
+                    );
+
+                    // If it exists, update the content, otherwise push the new report object to the array
+                    if (existingIndex >= 0) {
+                        reportList[existingIndex].content = report.content;
+
+                        // Then replace the button with the new one
+                        const selectedFiles = document.querySelectorAll(
+                            '.selected-file'
+                        );
+                        selectedFiles.forEach((file) => {
+                            if (file.innerText === report.fileName) {
+                                file.remove();
+                            }
+                        });
+                    } else {
+                        reportList.push(report);
+                    }
+
+                    // Save the updated reportList to local storage
+                    localStorage.setItem(
+                        'reportList',
+                        JSON.stringify(reportList)
+                    );
+
+                    const fileEl = createTemplateBtn(report);
+                    selectedFilesContainer.appendChild(fileEl);
+                };
+            }
+        };
+
+        input.click();
+    });
+}
